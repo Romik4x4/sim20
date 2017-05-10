ed044a282429175ea705889850888bad377a721e
Romik4x4 <Romik4x4@users.noreply.github.com>
2017-05-10 15:09:20 +0300

    Create SIM20.ino
    ok

---
+++ SIM20.ino
@@ -0,0 +1,12 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// If using software SPI (the default case):
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_CS    12
+#define OLED_RESET 13
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
